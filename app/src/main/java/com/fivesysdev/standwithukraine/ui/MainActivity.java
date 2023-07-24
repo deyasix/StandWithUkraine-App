297838a9c392917921506763c563d4be485b757a
deyasix <akikidzhan@gmail.com>
2023-07-24 16:16:40 +0300

    add statistic fragment

--- app/src/main/java/com/fivesysdev/standwithukraine/ui/MainActivity.java
+++ app/src/main/java/com/fivesysdev/standwithukraine/ui/MainActivity.java
@@ -1,115 +1,31 @@
 package com.fivesysdev.standwithukraine.ui;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.OrientationHelper;
-
-import android.content.res.Configuration;
-import java.time.LocalDate;
 import android.os.Bundle;
-import android.util.Pair;
 
 import com.fivesysdev.standwithukraine.R;
-import com.fivesysdev.standwithukraine.data.DayStatistic;
 import com.fivesysdev.standwithukraine.databinding.ActivityMainBinding;
-import com.fivesysdev.standwithukraine.mvp.Contract;
-import com.fivesysdev.standwithukraine.mvp.StatisticModel;
-import com.fivesysdev.standwithukraine.mvp.StatisticPresenter;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 
-public class MainActivity extends AppCompatActivity implements Contract.View {
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-
-    Contract.Presenter presenter;
+    private StatisticFragment statisticFragment;
+    private static final String STATISTIC_FRAGMENT_TAG = "STATISTIC_FRAGMENT_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        statisticFragment = (StatisticFragment) getSupportFragmentManager().findFragmentByTag(STATISTIC_FRAGMENT_TAG);
+        if (statisticFragment == null) {
+            statisticFragment = new StatisticFragment();
+        }
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fcv, statisticFragment, STATISTIC_FRAGMENT_TAG)
+                .commit();
         setContentView(binding.getRoot());
-        presenter = new StatisticPresenter(this, new StatisticModel());
-        setListeners();
-        setRecyclerView();
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString("date", binding.textViewDate.getText().toString());
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        presenter.setDate(savedInstanceState.getString("date"));
-    }
-
-    private void setListeners() {
-        binding.buttonPrevious.setOnClickListener(v -> presenter.onPreviousButtonClick());
-        binding.buttonNext.setOnClickListener(v -> presenter.onNextButtonClick());
-    }
-
-    private void setRecyclerView() {
-        setLayoutManager();
-        setAdapter();
-        setRecyclerDividers();
-        setEmptyDataObserver();
-    }
-
-    @Override
-    public void setDayStatistic(DayStatistic statistic) {
-        if (statistic != null) {
-            binding.textViewDate.setText(statistic.getDate());
-            binding.recyclerView.setAdapter(new StatisticAdapter(statistic.getStatistic()));
-        }
-        else binding.textViewDate.setText(LocalDate.now().toString());
-        setEmptyDataObserver();
-    }
-
-    @Override
-    public void blockingNextButton(boolean isBlocked) {
-        binding.buttonNext.setEnabled(!isBlocked);
-    }
-
-    private void setRecyclerDividers() {
-        DividerItemDecoration verticalDivider = new DividerItemDecoration(binding.recyclerView.getContext(),
-                OrientationHelper.VERTICAL);
-        DividerItemDecoration horizontalDivider = new DividerItemDecoration(binding.recyclerView.getContext(),
-                OrientationHelper.HORIZONTAL);
-        binding.recyclerView.addItemDecoration(verticalDivider);
-        binding.recyclerView.addItemDecoration(horizontalDivider);
-    }
-    private void setEmptyDataObserver() {
-        EmptyStatisticDataObserver emptyDataObserver =
-                new EmptyStatisticDataObserver(binding.recyclerView, findViewById(R.id.emptyData));
-        Objects.requireNonNull(binding.recyclerView.getAdapter()).registerAdapterDataObserver(emptyDataObserver);
-    }
-    private void setLayoutManager() {
-        GridLayoutManager layoutManager;
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
-            layoutManager = new GridLayoutManager(this, 2);
-        } else {
-            layoutManager = new GridLayoutManager(this, 3);
-        }
-        binding.recyclerView.setLayoutManager(layoutManager);
-    }
-    private void setAdapter() {
-        DayStatistic currentDayStatistic = presenter.getDayStatistic();
-        List<Pair<Integer, Integer>> stats;
-        if (currentDayStatistic == null) {
-            stats = new ArrayList<>();
-            binding.textViewDate.setText(LocalDate.now().toString());
-        } else {
-            binding.textViewDate.setText(currentDayStatistic.getDate());
-            stats = currentDayStatistic.getStatistic();
-        }
-        binding.recyclerView.setAdapter(new StatisticAdapter(stats));
-    }
 }
